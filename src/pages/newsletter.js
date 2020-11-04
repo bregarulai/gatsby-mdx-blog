6bec2c9f35d9147e800465677c3365d4d584b399
bregarulai <bregarulai@gmail.com>
2020-11-04 14:11:23 -0500

    setup form to collect data for netlify and created success page

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -7,7 +7,16 @@ const NewsLetter = () => {
         <div className="page-center">
           <h2>Get all the latest stories to your inbox</h2>
           <h4>I write to my digital friends every few weeks</h4>
-          <form action="" className="contact-form">
+          <form
+            action="/success"
+            className="contact-form"
+            name="contact"
+            method="POST"
+            netlify-honeypot="bot-field"
+            data-netlify="true"
+          >
+            <input type="hidden" name="bot-field" />
+            <input type="hidden" name="form-name" value="contact" />
             <input
               type="text"
               name="name"
