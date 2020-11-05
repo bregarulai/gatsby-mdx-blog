c40da41aef40cbe14189f3cc02adc0d1db8d6ba6
bregarulai <bregarulai@gmail.com>
2020-11-05 11:33:02 -0500

    created home page query to have latest posts data

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,38 @@ import Layout from '../components/Layout'
 import Posts from '../components/Posts'
 import { graphql } from 'gatsby'
 import SEO from '../components/SEO'
-// ...GatsbyImageSharpFluid
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
   return (
     <Layout>
       <Hero showPerson />
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }, limit: 3) {
+      nodes {
+        excerpt
+        id
+        frontmatter {
+          title
+          author
+          category
+          readTime
+          slug
+          date(formatString: "MMMM Do, YYYY")
+          image {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
