aebb4bc2d09819a103f577508dc2e472a0bc4ca9
bregarulai <bregarulai@gmail.com>
2020-11-09 15:03:12 -0500

    created h2 and h4 for all mdx files

--- root-mdx.js
+++ root-mdx.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
+import { Headings } from './src/components/Complete'
 
-const components = {}
+const components = {
+  h2: Headings.myH2,
+  h4: Headings.myH4,
+}
 
 export const wrapMDX = ({ element }) => {
   return <MDXProvider components={components}>{element}</MDXProvider>
